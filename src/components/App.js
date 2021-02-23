74dd149185b92373824d596588cf28215cbbbaa2
kranuj3122 <kumaranuj22799@gmail.com>
2021-02-24 05:25:56 +0530

    hello

--- src/components/App.js
+++ src/components/App.js
@@ -4,42 +4,30 @@ import "../styles/App.css";
 const App = ({ slides }) => {
   let [index, setIndex] = useState(0);
   const onNext = () => {
-    setIndex((i) => i + 1);
+    setIndex(index + 1);
   };
   const onPrev = () => {
-    setIndex((i) => i - 1);
+    setIndex(index - 1);
   };
   const onRestart = () => {
-    setIndex((i) => 0);
+    setIndex(0);
   };
   return (
     <>
       <h1 data-testid="title">{slides[index].title}</h1>
       <p data-testid="text">{slides[index].text}</p>
       {index != 0 ? (
-        <button
-          data-testid="button-prev"
-          style={{ left: "5px", position: "absolute" }}
-          onClick={onPrev}
-        >
+        <button data-testid="button-prev" onClick={onPrev}>
           Prev
         </button>
       ) : null}
       {index !== 0 ? (
-        <button
-          data-testid="button-restart"
-          style={{ left: "600px", position: "absolute" }}
-          onClick={onRestart}
-        >
+        <button data-testid="button-restart" onClick={onRestart}>
           Restart
         </button>
       ) : null}
-      {index !== 4 ? (
-        <button
-          data-testid="button-next"
-          style={{ right: "5px", position: "absolute" }}
-          onClick={onNext}
-        >
+      {index !== slides.length - 1 ? (
+        <button data-testid="button-next" onClick={onNext}>
           Next
         </button>
       ) : null}
