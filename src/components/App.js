ca920d109b6e0fb6206d6c585a6d7525dbe3807a
kranuj3122 <kumaranuj22799@gmail.com>
2021-02-24 05:11:25 +0530

    hello

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,50 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
+const App = ({ slides }) => {
+  let [index, setIndex] = useState(0);
+  const onNext = () => {
+    setIndex((i) => i + 1);
+  };
+  const onPrev = () => {
+    setIndex((i) => i - 1);
+  };
+  const onRestart = () => {
+    setIndex((i) => 0);
+  };
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-testid="title">{slides[index].title}</h1>
+      <p data-testid="text">{slides[index].text}</p>
+      {index != 0 ? (
+        <button
+          data-testid="button-prev"
+          style={{ left: "5px", position: "absolute" }}
+          onClick={onPrev}
+        >
+          prev
+        </button>
+      ) : null}
+      {index !== 0 ? (
+        <button
+          data-testid="button-restart"
+          style={{ left: "600px", position: "absolute" }}
+          onClick={onRestart}
+        >
+          restart
+        </button>
+      ) : null}
+      {index !== 4 ? (
+        <button
+          data-testid="button-next"
+          style={{ right: "5px", position: "absolute" }}
+          onClick={onNext}
+        >
+          next
+        </button>
+      ) : null}
+    </>
+  );
+};
 
 export default App;
